c385526b3325dcac024b966c47be028cf945b801
Scott Barr <scottjbarr@gmail.com>
2021-08-04 10:25:00 +1000

    Added ParseJSONStrict

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/scottjbarr/web
 
 go 1.15
+
+require github.com/go-playground/validator/v10 v10.7.0
--- json.go
+++ json.go
@@ -2,9 +2,33 @@ package web
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
 func ParseJSON(r *http.Request, dst interface{}) error {
 	return json.NewDecoder(r.Body).Decode(dst)
+}
+
+func ParseJSONStrict(r *http.Request, dst interface{}) error {
+	contentTtype := r.Header.Get("Content-Type")
+
+	if contentTtype != "application/json" {
+		return errors.New("not application/json")
+	}
+
+	if err := ParseJSON(r, dst); err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	if err := decoder.Decode(dst); err != nil {
+		return err
+	}
+
+	return validator.New().Struct(dst)
 }
